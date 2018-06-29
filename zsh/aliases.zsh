cefe00d51eb81b7a5c3baa33bfe640792c6cd2b5
evan <elightcap12@aim.com>
2018-06-29 10:44:40 -0700

    aliases change

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -61,7 +61,25 @@ alias freespace='clear;echo "Drive      Size  Used  Avail Use  Mounted on";df -h
 alias magnet-to-torrent='aria2c -q --bt-metadata-only --bt-save-metadata'
 alias biggest='find -type f -printf '\''%s %p\n'\'' | sort -nr | head -n 40 | gawk "{ print \$1/1000000 \" \" \$2 \" \" \$3 \" \" \$4 \" \" \$5 \" \" \$6 \" \" \$7 \" \" \$8 \" \" \$9 }"'
 alias tardir='( ( D=`builtin pwd`; F=$(date +$HOME/`sed "s,[/ ],#,g" <<< ${D/${HOME}/}`#-%F.tgz); S=$SECONDS; tar --ignore-failed-read --transform "s,^${D%/*},`date +${D%/*}.%F`,S" -czPf "$"F "$D" && logger -s "Tarred $D to $F in $(($SECONDS-$S)) seconds" ) & )'
-
+ alias zshconfig="nano ~/.zshrc" alias ohmyzsh="nano ~/.oh-my-zsh" alias 111="ssh elightcap@irv-1-1-sw01" alias 121="ssh elightcap@irv-1-2-sw01" alias 131="ssh elightcap@irv-1-3-sw01" alias 211="sshelightcap@irv-2-1-sw01" alias 221="ssh elightcap@irv-2-2-sw02" alias 311="ssh elightcao@irv-3-1-sw01" alias 321="ssh elightcap@irv-3-2-sw01" alias 322="ssh elightcap@irv-3-2-sw02" alias 323="ssh elightcap@irv-3-2-sw03" alias 411="ssh elightcap@irv-4-1-sw01" alias 511="sshelightcap@irv-5-1-sw01" alias 512="ssh elightcap@irv-5-1-sw02" alias 611="ssh elightcap@irv-6-1-sw01"
+ alias 612="ssh elightcap@irv-6-1-sw02"
+ alias 621="ssh elightcap@irv-6-2-sw01"
+ alias 622="ssh elightcap@irv-6-2-sw02"
+ alias 711="ssh elightcap@irv-7-1-sw01"
+ alias 712="ssh elightcap@irv-7-1-sw02"
+ alias 811="ssh elightcap@irv-8-1-sw01"
+ alias 911="ssh elightcap@irv-9-1-sw01"
+ alias 1011="ssh elightcap@irv-10-1-sw01"
+ alias 1111="ssh elightcap@irv-11-1-sw01"
+ alias 1311="ssh elightcap@irv-13-1-sw01"
+ alias 1312="ssh elightcap@irv-13-1-sw02"
+ alias 1321="ssh elightcap@irv-13-2-sw01"
+ alias 1322="ssh elightcap@irv-13-2-sw02"
+ alias 1411="ssh elightcap@irv-14-1-sw01"
+ alias 2011="ssh elightcap@irv-20-1-sw01"
+ alias 2111="ssh elightcap@irv-21-1-sw01"
+ alias 2211="ssh elightcap@irv-22-1-sw01"
+ alias 2212="ssh elightcap@irv-22-1-sw02"
 # Suffix alias
 alias -s jpg=sxiv
 alias -s png=sxiv
